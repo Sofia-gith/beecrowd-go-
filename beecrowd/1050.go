502df7412f524b7e79ccec260ed2212040da1bee
sofia <sofiafamorim@gmail.com>
2026-03-01 22:23:43 -0300

    feat: adicionando exercicio pares

---
+++ beecrowd/1050.go
@@ -0,0 +1,16 @@
+package beecrowd
+
+import(
+	"fmt"
+)
+
+// numeros pares
+
+func Pares(){
+	for i:=1; i<=100; i++{
+		if i%2==0{
+			fmt.Println(i)
+		}
+
+	}
+}
--- main.go
+++ main.go
@@ -17,6 +17,6 @@ func main() {
 	// bee.ImprimirRacionais()
 
 	// var c beecrowd.Cidade
-	beecrowd.Lanche()
+	beecrowd.Pares()
 
 }
