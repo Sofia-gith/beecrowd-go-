e4ff0097ad80f854132ceaac54abcb48fcd86c3d
sofia <sofiafamorim@gmail.com>
2026-06-15 16:20:37 -0300

    feat: Adicionando uma melhor solução utilizando hash map

--- LeetCode/twoSums.go
+++ LeetCode/twoSums.go
@@ -14,3 +14,22 @@ func twoSum(nums []int, target int) []int {
     }
     return nil
 }
+
+
+// Melhor solução
+
+func twoSumHashMap(nums []int, target int) []int {
+    seen := make(map[int]int) // valor -> índice
+
+    for i, num := range nums {
+        complement := target - num // Calculando o que é necessário
+
+        if j, ok := seen[complement]; ok { // Se ja foi visto o cmplemento
+            return []int{j, i} // então os indices j e i
+        }
+
+        seen[num] = i // se não achou ainda, registra a atual
+    }
+
+    return nil
+}
