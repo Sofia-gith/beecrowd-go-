29043065a2b879f3945a41672fd380c9f37fa343
sofia <sofiafamorim@gmail.com>
2026-06-15 16:11:12 -0300

    feat: Adicionando primeiro exercicio do leetCode

---
+++ LeetCode/twoSums.go
@@ -0,0 +1,16 @@
+package LeetCode
+
+import(
+
+)
+
+func twoSum(nums []int, target int) []int {
+    for i := 0; i < len(nums); i++ {
+        for j := i + 1; j < len(nums); j++ {
+            if nums[i] + nums[j] == target {
+                return []int{i, j}
+            }
+        }
+    }
+    return nil
+}
